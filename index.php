d10d01cac8c29d5e93046764f2c373c356e5b016
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-02-01 15:28:54 -0500

    fixes

--- index.php
+++ index.php
@@ -3,41 +3,70 @@
         include 'en-tete.html';
 
 
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            //require rather then include because the file is mandetory to run the 
+        require('connexion.php');
+        }
+        
         ?>
 
 
-<?php
-    echo "<form action='connexion.php' method='post'>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .main{
+            background-color:lightpink;
+            display:flex;
+            flex-direction: column;
+            justify-content: space-between;
+            padding: 20px; 
+            border-radius: 20px; 
+            width:50vw;
+            margin-left:200px;
+        }
+
+    </style>
+</head>
+<body>
     
-    echo "<div  style='background-color:lightpink; display:flex; 
-    flex-direction: column; justify-content: space-between;
-    padding: 20px; border-radius: 20px;'>";
+    <form action='connexion.php' method='post'>
+    
+        <div class="main" >
+        
+            <label for='prenom'>Prenom:</label>
+            <input type='text' id='prenom' name='prenom' required>
+
+            <label for='nom'>Nom de famille:</label>
+            <input type='text' id='nom' name='nom' required>
+
+            <label for='couriel'>Courriel:</label>
+            <input type='couriel' id='couriel' name='couriel' required>
+
+            <label for='psw'>Mot de Passe :</label>
+            <input type='password' id='psw' name='psw' required>
+
+            <label for='psw2'>Confirmer le mot de passe:</label>
+            <input type='password' id='psw2' name='psw2' required>
+
+            <section>
+                <input type='submit' style='width:20vw;' value="S'inscrire">
+                <input type='reset' style='width:20vw;' value="Effacer">
+            </section>
+
+        </div>
+
+    </form>
+
+</body>
+</html>
        
-       echo "<label for='prenom'>Prenom:</label>";
-        echo "<input type='text' id='prenom' name='prenom' required>";
-
-        echo "<label for='nom'>Nom de famille:</label>";
-       echo  "<input type='text' id='nom' name='nom' required>";
-
-       echo "<label for='couriel'>Courriel:</label>";
-        echo "<input type='couriel' id='couriel' name='couriel' required>";
-
-        echo "<label for='psw'>Mot de Passe :</label>";
-        echo "<input type='password' id='psw' name='psw' required>";
-
-       echo "<label for='psw2'>Confirmer le mot de passe:</label>";
-        echo "<input type='password' id='psw2' name='psw2' required>";
-               echo "<section>"; 
-       echo "<input type='submit' style='width:40vw;';>";
-        echo "<input type='reset' style='width:40vw;'>";
-    echo "</section>";
-
-        echo "</div>";
-
-       echo  "</form>";
-
-
-        ?>
+        
 
         <?php
 
